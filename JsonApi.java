880a93eb82dbd0b93da91c431abb514769ab306c
Šimo Matijević <simo.matijevic@gmail.com>
2018-11-26 05:59:26 +0100

    New files to repo

---
+++ JsonApi.java
@@ -0,0 +1,55 @@
+package cryptoConvert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.Iterator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.simple.parser.*; 
+
+ public class JsonApi {
+	
+	String output = "";
+    StringBuffer odgovor = new StringBuffer();
+   	
+    	public  StringBuffer metoda (String valutaIz,String valutaU) throws IOException{
+    		URL url = null ;
+    		url = new URL("https://min-api.cryptocompare.com/data/price?fsym="+valutaIz+"&tsyms="+valutaU);
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            if (connect.getResponseCode() != 200) {
+                throw new RuntimeException("Pogreška : HTTP status-kod : "
+                        + connect.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+            (connect.getInputStream())));
+             //String output;
+            System.out.println("Izlaz sa servera .... \n");
+             while ((output = br.readLine()) != null) {
+            	 odgovor.append(output);
+             }
+             br.close(); 
+             return odgovor;
+    	}
+    	public Double jsonRukovanje(String odgovor,String unos) throws ParseException{ 
+    		 String odgovorSt = odgovor.toString();
+             JSONParser parser = new JSONParser();
+             Object ob = parser.parse(odgovorSt);
+             JSONObject obj = (JSONObject)ob;   
+             Double valuta =  (Double) obj.get(unos);
+             return valuta;
+             
+    	}
+}
---
+++ Main.java
@@ -0,0 +1,44 @@
+package cryptoConvert;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.json.simple.parser.ParseException;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		
+		JsonApi japi = new JsonApi();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Credit for used API : https://min-api.cryptocompare.com/");
+		System.out.println("Unesite oznaku kriptovalute npr.(BTC)");
+		String kriptoUnos = sc.next().toUpperCase();
+		System.out.println("Unesite oznaku odredišne valute npr(USD)");
+		String valuta = sc.next().toUpperCase();
+		try{
+			String odgovorServera = japi.metoda(kriptoUnos,valuta).toString();
+			Double rezultatParsiranja = japi.jsonRukovanje(odgovorServera,valuta);
+			if(rezultatParsiranja == null){
+				System.out.println("Greška prilikom parsiranja, molimo provjerite Vaš unos!");
+			}else{
+				System.out.println(rezultatParsiranja);
+			}
+		}catch(Exception E){
+			System.out.println("Došlo je do pogreške");
+		}
+	}
+}
+
+
+
+
+
+
+//String provjera = japi.metoda(unos1, unos2).toString();
+/*
+		if(japi.metoda(unos1, unos2).toString().contains(("Response"))){
+			System.out.println("Pronađena je greška prilikom parsiranja, molimo provjerite unos valuta");
+		}
+		else{
+		*/
